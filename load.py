7510d2ae59a76f1af31b6301a120b069d6f3df94
rchitta2205 <rchitta@infoblox.com>
2021-08-15 14:50:56 -0400

    This commit is responsible for the following tasks:
    1) Adding a malware link datatset, which consists of links from 5 different categories - benign, malware, spam, phishing, defaced.
    
    2) Added a python script to parse the dataset and populate that in init-db.js which would be required to initialize our database

---
+++ load.py
@@ -0,0 +1,26 @@
+
+if __name__ == "__main__":
+
+    files = {"benign.csv": ["Nil", "Benign"], "defaced.csv": ["Medium", "Defaced"],
+             "malware.csv": ["High", "Malware"], "phishing.csv": ["Medium", "Phishing"],
+             "spam.csv": ["Low", "Spam"]}
+
+    outfile = "init-db.js"
+    template = '''
+db.malware.insert({{
+    \"url\": \"{0}\",
+    \"risk\": \"{1}\",
+    \"category\": \"{2}\"
+}});
+    '''
+
+    with open(outfile, 'a') as outFile:
+        for file in files:
+            infile = "url_dataset/" + file
+            risk = files[file][0]
+            category = files[file][1]
+            print(infile)
+            with open(infile, 'r', encoding='utf-8') as inFile:
+                for url in inFile:
+                    sanitisedUrl = url.strip().replace("\"", "").replace("'", "").replace("\\", "")
+                    outFile.write(template.format(sanitisedUrl, risk, category))
